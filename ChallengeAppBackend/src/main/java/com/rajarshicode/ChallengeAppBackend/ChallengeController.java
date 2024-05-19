a6420bb421b8293dc6df07cbd6ad1d17f5ed1757
RajarshiCode <rajarshidas47@gmail.com>
2024-05-19 09:40:37 +0530

    created the post challenge api, posted json data through postman and tested the get challnege api too,

--- ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeController.java
+++ ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeController.java
@@ -1,6 +1,8 @@
 package com.rajarshicode.ChallengeAppBackend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,4 +24,12 @@ public class ChallengeController {
     public List<Challenge> getAllChallenges(){
         return challenges;
     }
+
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge){
+        challenges.add(challenge);
+        return "challenge added succesfully";
+    }
+
 }
