5a48a6da8415a19fcecff33b36fca814950451b7
RajarshiCode <rajarshidas47@gmail.com>
2024-05-19 09:11:14 +0530

    added Challenge.java and ChallengeController.java

--- ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeAppApplication.java
+++ ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeAppApplication.java
@@ -3,9 +3,13 @@ package com.rajarshicode.ChallengeAppBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // tells us that this is an actual spring boot file
 public class ChallengeAppApplication {
 
+
+
+
+	// this is the main method that is actually responsible to run application
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeAppApplication.class, args);
 	}
---
+++ ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeController.java
@@ -0,0 +1,25 @@
+package com.rajarshicode.ChallengeAppBackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class ChallengeController {
+    private List<Challenge> challenges = new ArrayList<>();
+
+
+    public ChallengeController(){
+        Challenge challenge1 = new Challenge(1L, "May", "learn german");
+        challenges.add(challenge1);
+    }
+
+
+    @GetMapping("/challenges")
+    public List<Challenge> getAllChallenges(){
+        return challenges;
+    }
+}
