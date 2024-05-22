f04f7cfa20dccbf051b5d0a1dc84e97322874312
RajarshiCode <rajarshidas47@gmail.com>
2024-05-22 19:21:19 +0530

    completed database part
    test the crud operations using h2 database through Postman
    Working fine.
    MIgrated from ArrayList to actual database

--- ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeRepository.java
+++ ChallengeAppBackend/src/main/java/com/rajarshicode/ChallengeAppBackend/ChallengeRepository.java
@@ -2,8 +2,10 @@ package com.rajarshicode.ChallengeAppBackend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
 
-
+    Optional<Challenge> findByMonthIgnoreCase(String month);
 }
