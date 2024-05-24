0a6690b3e9c58ed928cd591aaa8e4e79722dc692
RajarshiCode <rajarshidas47@gmail.com>
2024-05-25 01:30:32 +0530

    Adding Challenges
    adding challenges from the frontend ui to the database and fetching the same in the page

--- challengeapp-frontend/src/App.js
+++ challengeapp-frontend/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import ChallengeList from './components/ChallengeList';
 import axios from 'axios';
+import AddChallenge from './components/AddChallenge';
 
 
 
@@ -12,21 +13,33 @@ function App() {
     
   ]);
 
+  const fetchChallenges = async () => {
 
-  useEffect(() =>{
-    const fetchChallenges = async () => {
+    try {
       const response = await axios.get('http://localhost:8080/challenges');
-      setChallenges(response.data);
+    setChallenges(response.data);
+    } catch (error) {
+      console.error("Error fetching challenges", error);
+    }
     
-    };
+  
+  };
+  useEffect(() =>{    
     fetchChallenges();
-  },[] )
+  },[] );
+
+
+  const handleChallengeAdded = () => {
+    fetchChallenges();
+  };
 
   return (
     <div className="App">
 
       <h1>Monthy Challenge</h1>
       {/* <ChallengeList></ChallengeList> */}
+
+      <AddChallenge onChallengeAdded={handleChallengeAdded}/>
 
       <ChallengeList challenges={challenges} />
 
